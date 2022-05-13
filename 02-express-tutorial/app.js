6f9d4de50d054df65cd069f43880b5251a8f709b
dobrga <dom.barga@gmail.com>
2022-05-13 21:34:02 +0800

    to finish

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,19 +5,34 @@ let { people } = require('./data.js')
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
-// app.use(express.json())
+app.use(express.json())
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({sucess: true, data: people})
 })
 
 app.post('/api/people', (req, res) => {
-  
+  const { name } = req.body
+  if(name) {
+    people.push({
+      id: people.length + 1,
+      name
+    })
+    console.log(people)
+    return res.status(200).send({
+      id: people.length + 1,
+      person: name
+    })
+  }
+  res.send('people heres')
 })
 
 app.post('/login', (req, res) => {
-  console.log(req.body)
-  res.send('posted')
+  const { name } = req.body
+  if(name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('give a name')
 })
 
 app.listen(5000, () => {
